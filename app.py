c6a97fffd7f40f80e8041781d6fb6268ecafc043
TohDeKai <tohdekai@outlook.com>
2023-02-05 14:54:16 +0800

    rename transaction month column

--- app.py
+++ app.py
@@ -9,19 +9,21 @@ st.set_page_config(page_title="HDB Resale Transactions")
 csv_file = 'resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.csv'
 df = pd.read_csv(csv_file)
 df['month'] = pd.to_datetime(df['month'], format="%Y-%m")
+df = df.rename(columns={'month': 'transaction_month'})
+
 # Selection
 flat_type = df['flat_type'].unique().tolist()
 town = df['town'].unique().tolist()
 lease_commence_date = df['lease_commence_date'].unique().tolist()
 
 # Filtering month
-month = df['month'].unique().tolist()
+transaction_month = df['transaction_month'].unique().tolist()
 
 # List of filters
-filter = ['flat_type','town','lease_commence_date','month']
+filter = ['flat_type','town','lease_commence_date','transaction_month']
 
 
-#Index(['month', 'town', 'flat_type', 'block', 'street_name', 
+#Index(['transaction_month', 'town', 'flat_type', 'block', 'street_name', 
 # 'storey_range','floor_area_sqm', 'flat_model', 
 # 'lease_commence_date','remaining_lease', 'resale_price'],dtype='object')
 
@@ -59,11 +61,11 @@ lease_commence_date_selection = st.slider('Lease Commence Date:',
 
 # Filtering by transaction date
 format = "YYYY-MM"
-start_date = df['month'].min()
+start_date = df['transaction_month'].min()
 start_date = start_date.to_pydatetime()
-end_date = df['month'].max()
+end_date = df['transaction_month'].max()
 end_date = end_date.to_pydatetime()
-month_selection = st.slider('Transaction Date:',
+transaction_month_selection = st.slider('Transaction Date:',
                                           min_value=start_date,
                                           max_value=end_date,
                                           value=(start_date,end_date))
@@ -72,14 +74,14 @@ month_selection = st.slider('Transaction Date:',
 mask = (df['flat_type'].isin(flat_type_selection) 
         &df['town'].isin(town_selection)
         &df['lease_commence_date'].between(*lease_commence_date_selection)
-        &df['month'].between(*month_selection))
+        &df['transaction_month'].between(*transaction_month_selection))
 number_of_result = df[mask].shape[0]
 st.markdown(f'*Available Results: {number_of_result}*')
 
 df_filtered = df[mask]
 
 try:
-    if filter_selection == "lease_commence_date" or filter_selection == 'month':
+    if filter_selection == "lease_commence_date" or filter_selection == 'transaction_month':
         chart = px.line(df_filtered,
                         x=df_filtered.groupby(filter_selection).median()['resale_price'].index,
                         y=df_filtered.groupby(filter_selection).median()['resale_price'],
